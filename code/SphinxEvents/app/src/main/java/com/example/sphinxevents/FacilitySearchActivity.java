1a0772e2db2d76479ae9ce23dc8812158636b7a5
Daniel Paunovic <paunovicdaniel045@gmail.com>
2024-11-04 17:27:32 -0700

    Implemented search and remove facility function for administrators

--- code/SphinxEvents/app/src/main/java/com/example/sphinxevents/FacilitySearchActivity.java
+++ code/SphinxEvents/app/src/main/java/com/example/sphinxevents/FacilitySearchActivity.java
@@ -1,15 +1,34 @@
 package com.example.sphinxevents;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class FacilitySearchActivity extends AppCompatActivity {
 
+    // Attributes for result list
+    private TextView noResultsTextView;
+    private ListView facilityList;
+    private FacilityAdapter facilityAdapter;
+
+    // Attributes for updating list
+    private ActivityResultLauncher<Intent> removeFacilityLauncher;
+    public static final int FACILITY_REMOVED = 1;
+    private Facility recentlyClickedFacility;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +41,83 @@ public class FacilitySearchActivity extends AppCompatActivity {
             return insets;
         });
 
-        // TODO: Get search query passed as Intent.putExtra();
-        String query = "Daniel's Facility";
+        // Obtains query string passed through intent
+        String query = "";
+        Intent intent = getIntent();
+        if (intent.hasExtra("query")) {
+            query = intent.getStringExtra("query");
+        }
 
+        // Obtains XML elements
+        facilityList = findViewById(R.id.facilities_list_view);
+        noResultsTextView = findViewById(R.id.no_results_text_view);
 
+        // Sets onClick listener for back button
+        ImageButton backButton = findViewById(R.id.search_facility_back_btn);
+        backButton.setOnClickListener(v -> {
+            finish();  // Go back to main search screen
+        });
 
+        // Displays search results
+        DatabaseManager databaseManager = DatabaseManager.getInstance();
+        databaseManager.searchFacility(query, new DatabaseManager.FacilitySearchCallback() {
+            @Override
+            public void onSuccess(ArrayList<Facility> facilities) {
+                if (facilities.isEmpty()) {
+                    setNoResultsDisplay();  // No results found
+                }
+                else {  // displays results
+                    facilityAdapter = new FacilityAdapter(FacilitySearchActivity.this, facilities);
+                    facilityList.setAdapter(facilityAdapter);
+
+                    // Sets up updating list if facility is removed
+                    removeFacilityLauncher = registerForActivityResult(
+                        new ActivityResultContracts.StartActivityForResult(),
+                        result -> {
+                            // Called when RemoveFacilityActivity returns
+                            if (result.getResultCode() == FACILITY_REMOVED) {  // If facility removal occurred
+                                refreshDisplay();  // update list display
+                            }
+                        }
+                    );
+
+                    // Clicking facility
+                    facilityList.setOnItemClickListener((parent, view, position, id) -> {
+                        recentlyClickedFacility = (Facility) parent.getItemAtPosition(position);
+                        Intent removeFacilityIntent = new Intent(FacilitySearchActivity.this, RemoveFacilityActivity.class);
+                        removeFacilityIntent.putExtra("facility", recentlyClickedFacility);
+                        removeFacilityLauncher.launch(removeFacilityIntent);
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                Toast.makeText(getApplicationContext(), "An error occurred.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
+
+    /**
+     * Shows proper display when no results found
+     */
+    public void setNoResultsDisplay() {
+        facilityList.setVisibility(View.GONE);
+        noResultsTextView.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Refreshes the display after a facility is removed
+     */
+    public void refreshDisplay() {
+        facilityAdapter.remove(recentlyClickedFacility);  // Remove the clicked facility
+
+        if (facilityAdapter.getCount() == 0) {  // Check if the results list is now empty
+            setNoResultsDisplay();
+        }
+        else {
+            facilityAdapter.notifyDataSetChanged();  // Refresh the results list display
+        }
     }
 }
